3d2880b3c4841a0a7375dfcdf1c9c0d7b9edd829
Jonas <70096814+Cormochamelion@users.noreply.github.com>
2021-04-11 22:14:00 +0200

    added first two tasks of execise 1

---
+++ exc_1.R
@@ -0,0 +1,61 @@
+
+# Task 1: Colwise matrix sums ---------------------------------------------
+
+matrix_sum <- function(A) {
+  
+  # if x is not already a matrix, check if it is coercible to one
+  # (raises an error is x is not a matrix and not coercible)
+  x <- as.matrix(A)
+  
+  # apply sum function to all columns
+  sums <- apply(A, 2, sum)
+  
+  return(sums)
+  
+}
+
+# demonstrate function works
+
+A <- matrix(rnorm(81), ncol = 9)
+
+res_func <- matrix_sum(A)
+
+res_rfunc <- colSums(A)
+
+all(res_func == res_rfunc)
+
+# Task 2: Check if matrix is symmetric ------------------------------------
+
+matrix_is_symmetric <- function(A) {
+  
+  # if x is not already a matrix, check if it is coercible to one
+  # (raises an error is x is not a matrix and not coercible)
+  x <- as.matrix(A)
+  
+  # check if all dimensions are even equal
+  if (!all(dim(A) == dim(A)[1])) {
+    
+    return(FALSE)
+    
+  }
+  
+  # check if the upper triangular is the same as the lower triangular
+  is_sym <- all(Matrix::tril(A) == t(as.matrix(triu(A))))
+  
+  return(is_sym)
+  
+}
+
+# demonstrate function works
+
+rand_res_func <- matrix_is_symmetric(A)
+
+# construct symmetric matrix from A and check that
+sym_res_func <- matrix_is_symmetric(Matrix::forceSymmetric(A))
+
+rand_res_rfunc <- Matrix::isSymmetric(A)
+sym_res_rfunc <- Matrix::isSymmetric(Matrix::forceSymmetric(A))
+
+all(rand_res_func == rand_res_rfunc, sym_res_func == sym_res_rfunc)
+
+
