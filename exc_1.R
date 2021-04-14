d9d33e9f9eb1b4b46786afff88061bf448354ad3
Jonas <jonas@pop-os.localdomain>
2021-04-14 18:24:25 +0200

    Attempt for task 3

--- exc_1.R
+++ exc_1.R
@@ -40,7 +40,7 @@ matrix_is_symmetric <- function(A) {
   }
   
   # check if the upper triangular is the same as the lower triangular
-  is_sym <- all(Matrix::tril(A) == t(as.matrix(triu(A))))
+  is_sym <- all(Matrix::tril(A) == t(as.matrix(Matrix::triu(A))))
   
   return(is_sym)
   
@@ -58,4 +58,54 @@ sym_res_rfunc <- Matrix::isSymmetric(Matrix::forceSymmetric(A))
 
 all(rand_res_func == rand_res_rfunc, sym_res_func == sym_res_rfunc)
 
+
+# Matrix to a power -------------------------------------------------------
+
+matrix_power <- function(A, k) {
+  
+  # if x is not already a matrix, check if it is coercible to one
+  # (raises an error is x is not a matrix and not coercible)
+  x <- as.matrix(A)
+  
+  # check if all dimensions are even equal
+  if (!all(dim(A) == dim(A)[1])) {
+    
+    stop('Matrix is not symmetric!')
+    
+  }
+  
+  # matrix to the power of 0 gives its identity matrix
+  if (k == 0) {
+    
+    return(diag(dim(A)[1]))
+    
+  }
+  
+  for (i in 0:floor(k / 2)) {
+    
+    if (i < 1) {
+      
+      M_sq <- A
+      
+    } else {
+      
+      M <- M_sq
+      
+      M_sq <- M %*% M
+      
+    }
+    
+  }
+  
+  if (k > 1 && k - i * 2 != 0) {
+    
+    print("here")
+    
+    M_sq <- M_sq %*% A
+    
+  }
+  
+  return(M_sq)
+  
+}
 
