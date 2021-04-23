61e23252fc7e2b176f8e00edb2024ffc01116c75
freimuth <freimuth@uni-potsdam.de>
2021-04-23 13:38:06 +0200

    Working until task 3, adapted task 2 for easier use of function in task 3

--- ACN_hw02_JonasFreimuth_807218.R
+++ ACN_hw02_JonasFreimuth_807218.R
@@ -23,6 +23,20 @@ getDegreesUndirected <- function(graph) {
 
 # Task 2 ------------------------------------------------------------------
 
+getNeighborsAdjacencySimple <- function (node, adj_mat, named = FALSE) {
+  row <- adj_mat[node, ]
+  
+  if (named) {
+    nb <- names(row[row != 0])
+    
+  } else {
+    nb <- which(row != 0)
+  }
+  
+  return(nb)
+  
+}
+
 getNeighborsUndirected <- function(graph, node) {
   if (!require("igraph", quietly = TRUE)) {
     stop("Package 'igraph' required but not installed.")
@@ -34,18 +48,66 @@ getNeighborsUndirected <- function(graph, node) {
   
   adj_mat <- as.matrix(graph[])
   
-  row <- adj_mat[node,]
-  
-  if (is_named(graph)) {
-    nb <- names(row[row != 0])
-    
-  } else {
-    nb <- which(row != 0)
-  }
+  nb <- getNeighborsAdjacencySimple(node, adj_mat)
   
   return(nb)
   
 }
+
+
+# Task 3 ------------------------------------------------------------------
+
+getNthNeighborsUndirected <- function(graph, v, order, simplify = TRUE) {
+  if (!require("igraph", quietly = TRUE)) {
+    stop("Package 'igraph' required but not installed.")
+  }
+  
+  if (is_directed(graph)) {
+    stop("This function can only deal with undirected graphs.")
+  }
+  
+  adj_mat <- as.matrix(graph[])
+  
+  nodes <- list()
+  
+  # initialize vector of already found neighbors with 0
+  # empty vector causes problems at initial comparing
+  anc_nds <- c(0)
+  
+  for (i in 1:order) {
+    if (i == 1) {
+      # handle case of i == 1, use original node
+      prev_nodes <- v
+      
+    } else {
+      # every other case: the previous nodes are in the previous list entry
+      prev_nodes <- nodes[[i - 1]]
+    }
+    
+    # get list of each of the previous neighbors neighbors
+    # puts a list at pos i in the nodes list
+    nodes[[i]] <- lapply(prev_nodes, getNeighborsAdjacencySimple, adj_mat)
+    
+    # convert output to a vector and filter out duplicates and nodes 
+    # previously visited
+    nodes[[i]] <- unique(unlist(nodes[[i]]))
+    nodes[[i]] <- nodes[[i]][!(nodes[[i]] %in% anc_nds)]
+    
+    # update list of visited nodes
+    anc_nds <- unlist(nodes)
+  }
+  
+  # if requested, turn output into a vector
+  if (simplify) {
+    nodes <- unlist(nodes)
+  }
+  
+  return(nodes)
+  
+}
+
+
+# Task 4 ------------------------------------------------------------------
 
 
 
@@ -73,9 +135,17 @@ if (sys.nframe() == 0) {
   gNU_neighbor <- getNeighborsUndirected(rnd_graph, rnd_vert)
   igraph_neighbor <- neighbors(rnd_graph, rnd_vert)
   
-  if (all(gNU_neighbor == igraph_neighbor)) {
+  if (all(gNU_neighbor %in% igraph_neighbor)) {
     print("Task 2 works.")
   }
   
+  # task 3
+  
+  gNNU_neighbors <- getNthNeighborsUndirected(rnd_graph, rnd_vert, 2)
+  igraph_nth_neighbor <- unlist(ego(rnd_graph, nodes = rnd_vert, order = 2))
+  
+  if (all(gNNU_neighbors %in% igraph_nth_neighbor)) {
+    print("Task 3 works.")
+  }
   
 }
