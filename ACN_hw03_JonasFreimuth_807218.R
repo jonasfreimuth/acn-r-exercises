ffa9103ce1acb0a065c6d5e0e27a625b82083d7d
freimuth <freimuth@uni-potsdam.de>
2021-04-29 21:48:30 +0200

    Task 2 working

--- ACN_hw03_JonasFreimuth_807218.R
+++ ACN_hw03_JonasFreimuth_807218.R
@@ -1,12 +1,21 @@
 
-# Task 1 ------------------------------------------------------------------
+# Helper functions --------------------------------------------------------
 
-pwDistUnweightSimple <- function(v1, v2, G) {
+check_valid <- function (G) {
+  if (!require("igraph", quietly = TRUE)) {
+    stop("Package 'igraph' required but not installed.")
+  }
   
   if (is_weighted(G) || !is_simple(G)) {
     stop(paste("BFS algorithm to find pairwise shortest paths does not work on",
                "weighted graphs or multigraphs."))
   }
+}
+
+# Task 1 ------------------------------------------------------------------
+
+pwDistUnweightSimple <- function (G, v1, v2) {
+  check_valid(G)
   
   if (v1 == v2) {
     return(0)
@@ -47,9 +56,41 @@ pwDistUnweightSimple <- function(v1, v2, G) {
       }
     }
   }
-  return(Inf)
+  return (Inf)
 }
 
+# Task 2 ------------------------------------------------------------------
+
+# function to determine eccentricity of a node
+getEccUnweightSimple <- function (G, v) {
+  check_valid(G)
+  
+  weight <- rep(NA, length(V(G)))
+  for (w in V(G)[-v]) {
+    weight[w] <- pwDistUnweightSimple(G, v, w)
+  }
+  
+  return (max(weight, na.rm = TRUE))
+}
+
+# function to give nodes belonging to center
+getCenterUnweightSimple <- function (G) {
+  check_valid(G)
+  
+  eccs <- sapply(V(G), getEccUnweightSimple, G = G)
+  
+  return (which(eccs == min(eccs)))
+  
+}
+
+# function to plot graph with select nodes highlighted
+plotHighlight <- function (G, vtcs) {
+  V(G)$color <- NA
+  V(G)$color[vtcs] <- "gold"
+  V(G)$color[is.na(V(G)$color)] <- "darkgray"
+  
+  plot(G)
+}
 
 # Tests -------------------------------------------------------------------
 
@@ -70,11 +111,25 @@ if (sys.nframe() == 0) {
   
   # task 1
   
-  dist_pwDist <- pwDistUnweightSimple(rnd_vert_1, rnd_vert_2, rnd_graph)
+  dist_pwDist <- pwDistUnweightSimple(rnd_graph, rnd_vert_1, rnd_vert_2)
   dist_shrtPath <- shortest.paths(rnd_graph, rnd_vert_1, rnd_vert_2)[1,1]
   
   if (dist_pwDist == dist_shrtPath) {
     print("Task 1 works")
   }
+  
+  # task 2
+  
+  ecc_gEUS <- getEccUnweightSimple(rnd_graph, rnd_vert)
+  ecc_ecc <- eccentricity(rnd_graph, rnd_vert)
+  
+  if (ecc_ecc == ecc_gEUS) {
+    print("Task 2: Eccentricity determination works") 
+  }
+  
+  ctr_gCUS <- getCenterUnweightSimple(rnd_graph)
+  
+  plotHighlight(rnd_graph, ctr_gCUS)
+  
 }
 
