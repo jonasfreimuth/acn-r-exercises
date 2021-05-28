6f66c97128b8bc62bb60818e917f340d377c6b33
freimuth <freimuth@uni-potsdam.de>
2021-05-28 18:38:27 +0200

    Added task 2, apparently working

--- ACN_hw06_JonasFreimuth_807218.R
+++ ACN_hw06_JonasFreimuth_807218.R
@@ -15,6 +15,104 @@ checkValid <- function (G) {
   }
 }
 
+# transitivity from hw_04
+
+nTriangles <- function (graph, tri.out = FALSE) {
+  checkValid(graph)
+  
+  is_dir <- is.directed(graph)
+  
+  # if requested, initialize matrix for vertex ids each triangles
+  if (tri.out) { 
+    triangs <- matrix(nrow = length(V(graph))^3,
+                      ncol = 3) 
+  }
+  
+  count <- 0
+  
+  # for every node u
+  for (u in V(graph)) {
+    # in case of directed graph, a triangle consists of the edges 
+    # u -> v -> w -> u, with v an out-neighbor of u and w an in-neighbor of u
+    out_nbs <- neighbors(graph, u, mode = "out")
+    in_nbs <- neighbors(graph, u, mode = "in")
+    
+    # for every out-neighbor v of u
+    for (v in out_nbs) {
+      # for every in neighbor w of u
+      for (w in in_nbs) {
+        # if an edge connects v to w
+        if (graph[v, w]) {
+          count <- count + 1
+          # print(c(u, v, w))
+          if (tri.out) { triangs[count, ] <- c(u, v, w) }
+        }
+      }
+      
+      # delete the edge between u & v
+      # both from graph and the neighborhoods
+      graph[u, v] <- FALSE
+      if (!is_dir) { graph[v, u] <- FALSE }
+      out_nbs <- out_nbs[out_nbs != v]
+      if (!is_dir) { in_nbs <- in_nbs[in_nbs != v] }
+    }
+  }
+  
+  # prepare output
+  if (tri.out) {
+    out <- list(n = count,
+                triangles = triangs[!apply(triangs,
+                                           1,
+                                           function(x) any(is.na(x)))
+                                    ,])
+  } else {
+    out <- count
+  }
+  
+  return (out)
+  
+}
+
+# 2-Paths
+
+# for every node u
+#   for every in-neighbor v of u
+#     for every out-neighbor w of u
+#       increase count
+
+nTwoPath <- function(graph) {
+  checkValid(graph)
+  
+  is_dir <- is.directed(graph)
+  
+  count <- 0
+  
+  # for every node u
+  for (u in V(graph)) {
+    out_nbs <- neighbors(graph, u, mode = "out")
+    in_nbs <- neighbors(graph, u, mode = "in")
+    
+    # these formulas should always amount to the number of cycles in the for 
+    # loops from the pseudocode
+    if (is_dir) {
+      count <- count + length(in_nbs) * length(out_nbs)
+    } else {
+      count <- count + length(in_nbs) * (length(in_nbs) - 1) / 2
+    }
+  }
+  
+  return (count)
+}
+
+# transitivity function
+
+transtvty <- function (graph) {
+  checkValid(graph)
+  
+  return (3 * nTriangles(graph) / nTwoPath(graph))
+  
+}
+
 
 # Task 1 ------------------------------------------------------------------
 
@@ -45,10 +143,6 @@ bonacicCent <- function (G, alpha = 1, beta = 1) {
 
 # Task 2 ------------------------------------------------------------------
 
-fastTransitivity <- function (G) {
-  return(NA)
-}
-
 erdosRenyiOrder <- function (G) {
   checkValid(G) 
   
@@ -58,12 +152,12 @@ erdosRenyiOrder <- function (G) {
   m <- length(E(G))
   is_dir <- is.directed(G)
   
-  targ_trans <- fastTransitivity(G)
+  targ_trans <- transtvty(G)
   
   comp_trans <- rep(NA, repeats)
   
   for (i in 1:repeats) {
-    comp_trans[i] <- fastTransitivity(sample_gnm(n, m, is_dir))
+    comp_trans[i] <- transtvty(sample_gnm(n, m, is_dir))
   }
   
   comp_trans <- mean(comp_trans, na.rm = TRUE)
@@ -115,5 +209,14 @@ if (sys.nframe() == 0) {
   
   # Task 2 ---------------------------------------------------------------
   
+  time <- system.time(erdosOrder <- erdosRenyiOrder(rnd_graph1))[["elapsed"]]
+  
+  if (erdosOrder) {
+    print("Graph has higher transitivity than the average Erdős–Rényi graph.")
+  } else {
+    print("Graph has lower transitivity than the average Erdős–Rényi graph.")
+  }
+  
+  print(paste("It took", round(time), "seconds to find that out."))
   
 }
