5e2ec6ba228cfc336a8a52af6c0008a5047ca1cf
Jonas <jonas@pop-os.localdomain>
2021-06-03 20:54:04 +0200

    exercise 7 added, task 1 and 2 working

---
+++ exercise_07.R
@@ -0,0 +1,79 @@
+# I'm sorry, person reading this. I just had to do everything pre-assigned and 
+# pseudo-memory efficient, taking much longer to code this than I would have 
+# liked
+stubMatching <- function(degSeq, simple = TRUE) {
+  if (sum(degSeq) %% 2 != 0) {
+    stop ("Degree sequence not graphic")
+  }
+  
+  stub_vec <- rep(NA, sum(degSeq))
+  
+  i <- 1
+  j <- 1
+  while (i <= length(degSeq)) {
+    if (degSeq[i] < 1) {
+      break
+    }
+    
+    repl_ind <- j:(j + degSeq[i] - 1)
+    stub_vec[repl_ind] <- rep(i, degSeq[i])
+    
+    j <- j + degSeq[i]
+    i <- i + 1
+  }
+  
+  stub_vec <- stub_vec[!is.na(stub_vec)]
+  
+  G <- make_empty_graph(length(degSeq), directed = FALSE)
+  
+  for (i in seq(1, sum(degSeq), 2)) {
+    
+    redo_pair <- TRUE
+    while (redo_pair) {
+      
+      # generate distinct pairing indices, this is necessary to have for
+      # deleting the entries later
+      same <- TRUE
+      while (same) {
+        ind_1 <- round(runif(1, min = 1, max = length(stub_vec)))
+        ind_2 <- round(runif(1, min = 1, max = length(stub_vec)))
+        
+        if (ind_1 != ind_2) {
+          same <- FALSE
+        }
+      }
+      pair_indcs <- c(ind_1, ind_2)
+      
+      pair <- stub_vec[pair_indcs]
+      
+      # check if we want a simple graph
+      if (simple) {
+        # if so, check whether adding the current pair violates that
+        if (!(get.edge.ids(G, pair) > 0 || pair[1] == pair[2])) {
+          # if not, continue on
+          redo_pair <- FALSE
+        }
+      } else {
+        redo_pair <- FALSE
+      }
+    }
+    
+    G <- add.edges(G, pair)
+    
+    stub_vec <- stub_vec[-pair_indcs]
+  }
+  
+  return(G)
+}
+
+library(igraph)
+
+opar <- par(mfrow = c(1, 2))
+
+rnd_graph <- sample_gnp(runif(1, 1, 20), runif(1), directed = FALSE)
+plot(rnd_graph)
+
+rnd_rnd_graph <- stubMatching(sort(degree(rnd_graph), decreasing = TRUE))
+plot(rnd_rnd_graph)
+
+par(opar)
