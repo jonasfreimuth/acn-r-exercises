308605138a1666629656d51c44affdb7f3c17158
freimuth <freimuth@uni-potsdam.de>
2021-06-04 16:10:49 +0200

    Started fix but problem also present in main

--- exercise_07.R
+++ exercise_07.R
@@ -6,54 +6,71 @@ stubMatching <- function(degSeq, simple = TRUE) {
     stop ("Degree sequence not graphic")
   }
   
-  stub_vec <- rep(NA, sum(degSeq))
-  
-  i <- 1
-  j <- 1
-  while (i <= length(degSeq)) {
-    if (degSeq[i] < 1) {
-      break
-    }
+  if (sum(degSeq) <= 0) {
+    warning ("Resulting graph has no edges")
     
-    repl_ind <- j:(j + degSeq[i] - 1)
-    stub_vec[repl_ind] <- rep(i, degSeq[i])
-    
-    j <- j + degSeq[i]
-    i <- i + 1
+    G <- make_empty_graph(n = length(degSeq))
+    return (G)
   }
   
-  stub_vec <- stub_vec[!is.na(stub_vec)]
-  
-  G <- make_empty_graph(length(degSeq), directed = FALSE)
-  
-  for (i in seq(1, sum(degSeq), 2)) {
+  start_over <- TRUE
+  while (start_over) {
+    start_over <- FALSE
     
-    redo_pair <- TRUE
-    while (redo_pair) {
-      
-      pair_indcs <- sample(which(!is.na(stub_vec)), 2)
-      pair <- stub_vec[pair_indcs]
-      
-      # check if we want a simple graph
-      if (simple) {
-        # if so, check whether adding the current pair violates that
-        if (!(get.edge.ids(G, pair) > 0 || pair[1] == pair[2])) {
-          # if not, continue on
-          redo_pair <- FALSE
-        } else {
-          # TODO: the way this is done now it can get stuck on the last
-          # pair, which always violates simplicity. I dont know how to fix 
-          # this without starting over completely
-          print(pair)
-        }
-      } else {
-        redo_pair <- FALSE
+    stub_vec <- rep(NA, sum(degSeq))
+    
+    i <- 1
+    j <- 1
+    while (i <= length(degSeq)) {
+      if (degSeq[i] < 1) {
+        break
       }
+      
+      repl_ind <- j:(j + degSeq[i] - 1)
+      stub_vec[repl_ind] <- rep(i, degSeq[i])
+      
+      j <- j + degSeq[i]
+      i <- i + 1
     }
     
-    G <- add.edges(G, pair)
+    stub_vec <- stub_vec[!is.na(stub_vec)]
     
-    stub_vec[pair_indcs] <- NA
+    G <- make_empty_graph(length(degSeq), directed = FALSE)
+    
+    for (i in seq(1, sum(degSeq), 2)) {
+      
+      redo_pair <- TRUE
+      while (redo_pair) {
+        
+        pair_indcs <- sample(which(!is.na(stub_vec)), 2)
+        pair <- stub_vec[pair_indcs]
+        
+        # check if we want a simple graph
+        if (simple) {
+          # if so, check whether adding the current pair violates that
+          if (!(get.edge.ids(G, pair) > 0 || pair[1] == pair[2])) {
+            # if not, continue on
+            redo_pair <- FALSE
+          } else {
+            if (sum(!is.na(stub_vec)) <= 2) {
+              # sometimes this my get stuck on the last pair if that would
+              # violate graph simplicity
+              start_over <- TRUE
+              
+              # break out of the redo loop
+              break
+            }
+          }
+        } else {
+          redo_pair <- FALSE
+        }
+      }
+      
+      G <- add.edges(G, pair)
+      
+      stub_vec[pair_indcs] <- NA
+    }
+    
   }
   
   return(G)
