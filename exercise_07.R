72415ae91b21e2996d9c862ca48647b11a924a30
freimuth <freimuth@uni-potsdam.de>
2021-06-05 13:54:16 +0200

    Added number of tries to simple stubmatching, seems to work

--- exercise_07.R
+++ exercise_07.R
@@ -1,7 +1,24 @@
+
+# Helper functions --------------------------------------------------------
+
+# function to ensure all packages are available, and a given graph
+# does not violate any of the assumptions a function makes
+
+checkValid <- function (G) {
+  if (!require("igraph", quietly = TRUE)) {
+    stop("Package 'igraph' required but not installed.")
+  }
+  
+  if (is_weighted(G) || !is_simple(G)) {
+    stop(paste("This algorithm does not work on",
+               "weighted graphs or multigraphs."))
+  }
+}
+
 # I'm sorry, person reading this. I just had to do everything pre-assigned and 
 # pseudo-memory efficient, taking much longer to code this than I would have 
 # liked
-stubMatching <- function(degSeq, simple = TRUE) {
+stubMatching <- function(degSeq, simple = TRUE, n_tries = 50) {
   if (sum(degSeq) %% 2 != 0) {
     stop ("Degree sequence not graphic")
   }
@@ -14,7 +31,8 @@ stubMatching <- function(degSeq, simple = TRUE) {
   }
   
   start_over <- TRUE
-  while (start_over) {
+  k <- 1
+  while (start_over && k < n_tries) {
     start_over <- FALSE
     
     stub_vec <- rep(NA, sum(degSeq))
@@ -37,6 +55,10 @@ stubMatching <- function(degSeq, simple = TRUE) {
     
     G <- make_empty_graph(length(degSeq), directed = FALSE)
     
+    if (simple) {
+      wrong_pairs <- list()
+    }
+    
     for (i in seq(1, sum(degSeq), 2)) {
       
       redo_pair <- TRUE
@@ -47,23 +69,37 @@ stubMatching <- function(degSeq, simple = TRUE) {
         
         # check if we want a simple graph
         if (simple) {
+          
+          # if we already stamped this pair as wrong discard it right away
+          if (list(sort(pair)) %in% wrong_pairs) {
+            next
+          }
+          
           # if so, check whether adding the current pair violates that
           if (!(get.edge.ids(G, pair) > 0 || pair[1] == pair[2])) {
             # if not, continue on
             redo_pair <- FALSE
           } else {
-            if (sum(!is.na(stub_vec)) <= 2) {
-              # sometimes this my get stuck on the last pair if that would
-              # violate graph simplicity
-              start_over <- TRUE
-              
-              # break out of the redo loop
-              break
-            }
+            wrong_pairs[[toString(i)]] <- sort(pair)
           }
+          
+          if (length(wrong_pairs) >= sum(!is.na(stub_vec)) ) {
+            # sometimes this may get stuck on the last pair if that would
+            # violate graph simplicity
+            start_over <- TRUE
+            k <- k + 1
+            
+            # break out of the redo loop
+            break
+          }
+          
         } else {
           redo_pair <- FALSE
         }
+      }
+      
+      if (start_over) {
+        break
       }
       
       G <- add.edges(G, pair)
@@ -73,7 +109,12 @@ stubMatching <- function(degSeq, simple = TRUE) {
     
   }
   
-  return(G)
+  if (k >= n_tries) {
+    warning (paste("Could not find a solution satisfying the simple",
+                   "criterion, some nodes may be missing."))
+  }
+  
+  return (G)
 }
 
 library(igraph)
