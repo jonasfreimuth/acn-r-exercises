fe115eb6a84d345f1c25984086654235f8db3d53
freimuth <freimuth@uni-potsdam.de>
2021-08-02 12:14:30 +0200

    Added task 20 of Exam 2019

---
+++ Exam_2019_Task_20.R
@@ -0,0 +1,27 @@
+threeNodeSub <- function(G) {
+  nw_list <- list()
+  
+  for (u in V(G)) {
+    nbs <- neighbors(G, u)
+    
+    pairs <- t(combn(nbs, 2))
+    
+    for (i in 1:nrow(pairs)) {
+      nw_list[[length(nw_list) + 1]] <- c(u, pairs[i,])
+      print(c(u, pairs[i,]))
+    }  }
+  
+  return(nw_list)
+}
+
+if (sys.nframe() == 0) {
+  library(igraph)
+  
+  rnd_graph <- sample_gnp(20, 0.2, directed = FALSE)
+  
+  nws <- threeNodeSub(rnd_graph)
+  
+  # check if each nw is counted once
+  length(nws) == length(unique(nws))
+  
+}
